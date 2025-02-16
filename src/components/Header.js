dbce5ddff33c584fa9f2ed374c0cd9afd322383b
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2025-02-16 16:31:12 +0530

    Update and rename Header.jsx to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import React from 'react';
 function Header() {
     return (
         <h1 style={{ textAlign: 'center', background: 'pink', fontSize: '2rem', padding: '1rem', color: 'white', fontFamily: 'sans-serif' }}>RESUME GENERATOR</h1>

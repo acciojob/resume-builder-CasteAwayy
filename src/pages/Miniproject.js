31c5bd7a2320116f0aa37a7157c0f7111969c9b2
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2025-02-16 16:40:53 +0530

    Update Miniproject.js

--- src/pages/Miniproject.js
+++ src/pages/Miniproject.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navigation from "../components/Navigation";
 import { miniprojectCreated } from "../resumeSlice";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 function Miniproject({ pageNumber, setPageNumber }) {

9abecdb9cb674ec6a8e0e357188ed1bfad1b1b46
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2025-02-16 16:39:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Profile from "./pages/Profile";
 import Skills from "./pages/Skills";
